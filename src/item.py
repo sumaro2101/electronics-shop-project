854d655ca2f2d27fe8e5c02476a55229f577925a
Alex Pavlov <yukiu217@gmail.com>
2024-01-26 11:54:11 -0800

    added descount and tests

--- src/item.py
+++ src/item.py
@@ -1,15 +1,27 @@
 class Parametr:
+    """Дексриптор данных
+    """    
     
     @classmethod
     def __verify_param(cls, name: str, param) -> None:
+        """Верификация данных поступаемых в параметры класса
+
+        Args:
+            name (str): имя поля
+            param (any): значение поспающее в поле
+            
+            _name проверяет строку
+            _price проверят цисло и дробь а так же значение меньше нуля
+            _quantity проверят цисло и значение меньше нуля
+        """        
         
         if name == "_name":
             if not isinstance(param, str):
                 raise TypeError("Параметр 'Имя' ожидал строку")
             
         if name == "_price":
-            if not isinstance(param, float):
-                raise TypeError("Параметр 'Цена' ожидал цисло с плавающей точкой")
+            if not isinstance(param, int|float):
+                raise TypeError("Параметр 'Цена' ожидал цисло с плавающей точкой или целое число")
             if param <= 0.0:
                 raise ValueError("Параметр 'Цена' не может быть ноль или меньше нуля")
             
@@ -19,13 +31,24 @@ class Parametr:
             if param < 0:
                 raise ValueError("Параметр 'Количество' не может быть меньше нуля")
     
+    
     def __set_name__(self, owner, name: str) -> None:
+        """Модифицирует имя поля для инкапсуляции
+        """        
         self.name = "_" + name
         
+        
     def __get__(self, instance, owner) -> (str|float|int):
+        """Получения значения из поля
+        """   
+             
         return getattr(instance, self.name)
     
+    
     def __set__(self, instance, value) -> None:
+        """Назначение значения в поле
+        """    
+            
         self.__verify_param(self.name, value)
         setattr(instance, self.name, value)
             
@@ -35,6 +58,7 @@ class Item:
     """
     Класс для представления товара в магазине.
     """
+    
     pay_rate = 1.0
     all = []
     
@@ -60,7 +84,16 @@ class Item:
         self.name = name
         self.price = price
         self.quantity = quantity
+        
      
+    @classmethod
+    def __validate_discount(cls, cof: int|float):
+        if not isinstance(cof, int|float):
+            raise TypeError(f"Скидка: Ожидалось число или дробь, не {type(cof)}")
+        
+        if cof < 0:
+            raise ValueError("Скидка: Ожидалось значение больше нуля")
+        
         
     def calculate_total_price(self) -> float:
         """
@@ -76,5 +109,7 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        
+        self.__validate_discount(self.pay_rate)
+        self._price *= self.pay_rate
     
